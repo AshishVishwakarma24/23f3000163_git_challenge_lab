0d0923adc195a4433739178269a178616c73e2c8
Ashish <23f3000163@ds.study.iitm.ac.in>
2025-09-17 02:41:21 +0530

    feat: add diff_module.py for subtraction operation

---
+++ diff_module.py
@@ -0,0 +1,9 @@
+
+def subtract(a, b):
+    """Return the difference of two numbers."""
+    return a - b
+
+
+if __name__ == "__main__":
+    # Example run
+    print("Subtraction: 10 - 5 =", subtract(10, 5))
